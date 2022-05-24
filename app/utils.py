c2ce7a6c60a83c9a57490ca40e7c88047b0ba5ab
Paulo Moggi <paulohpmoggi@gmail.com>
2022-05-23 21:14:09 -0300

    feat: Creates a round function

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,25 @@
 import decimal
 from decimal import Decimal
+from typing import Union
 
 
-def get_inss_tax(salary: str, inss_range_rate: dict[str, str] = None) -> str:
+def round_money(amount: Decimal) -> Decimal:
+    """Utility function to round money
+
+    Examples
+    --------
+    >>> round_money(Decimal("0.1551"))
+    Decimal('0.15')
+    >>> round_money(Decimal("0.1555"))
+    Decimal('0.15')
+    >>> round_money(Decimal("0.1556"))
+    Decimal('0.16')
+    """
+    decimal.getcontext().rounding = decimal.ROUND_HALF_DOWN
+    return min(round((amount), 2), round(round(amount, 3), 2))
+
+
+def get_inss_tax(salary: Union[Decimal, str, int, float], inss_range_rate: dict[str, str] = None) -> Decimal:
     """Utility function to retrieve inss tax based on salary amount.
 
     Parameters
@@ -15,18 +32,19 @@ def get_inss_tax(salary: str, inss_range_rate: dict[str, str] = None) -> str:
     Examples
     --------
     >>> get_inss_tax("1200")
-    '90.00'
+    Decimal('90.00')
     >>> get_inss_tax("1212")
-    '90.90'
+    Decimal('90.90')
     >>> get_inss_tax("1212.89")
-    '90.98'
+    Decimal('90.98')
     >>> get_inss_tax("1212.90")
-    '90.98'
+    Decimal('90.98')
     >>> get_inss_tax("1300")
-    '98.82'
+    Decimal('98.82')
     >>> get_inss_tax("8070")
-    '828.38'
+    Decimal('828.38')
     """
+    salary = Decimal(salary)
     default_range_rate = {
         "1212.00": "7.5",
         "2427.35": "9",
@@ -36,28 +54,26 @@ def get_inss_tax(salary: str, inss_range_rate: dict[str, str] = None) -> str:
     final_range_rate = inss_range_rate or default_range_rate
     final_range_rate["-0.01"] = final_range_rate["-0.01"] if "-0.01" in final_range_rate else "0"
 
-    inss_range = list(sorted(final_range_rate.keys(), key=Decimal))
-    inss_rate = list(sorted(final_range_rate.values(), key=Decimal))
-
-    decimal.getcontext().rounding = decimal.ROUND_HALF_DOWN
+    inss_range = sorted(list(map(Decimal, final_range_rate.keys())))
+    inss_rate = sorted(list(map(Decimal, final_range_rate.values())))
 
     residual = 0
     for index in range(1, len(inss_range)):
         range_value = inss_range[index]
         range_tax = inss_rate[index]
 
-        lower_range = Decimal(inss_range[index - 1]) + Decimal("0.01")
+        lower_range = inss_range[index - 1] + Decimal("0.01")
 
-        if Decimal(salary) <= Decimal(range_value):
+        if salary <= range_value:
             if lower_range == Decimal("0.01"):
                 lower_range = Decimal("0")
 
-            partial = (Decimal(salary) - lower_range) * Decimal(range_tax) / Decimal(100)
-            residual += round(round(partial, 3), 2)
+            partial = (salary - lower_range) * range_tax / Decimal("100")
+            residual += round_money(partial)
             break
 
-        partial = (Decimal(range_value) - lower_range) * Decimal(range_tax) / Decimal(100)
-        residual += round(round(partial, 3), 2)
+        partial = (range_value - lower_range) * range_tax / Decimal("100")
+        residual += round_money(partial)
 
-    final = round(round(residual, 3), 2)
-    return str(final)
+    final = round_money(residual)
+    return final
