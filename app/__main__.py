5fff174265f30ef0f89c401132f75640309d1c4b
Paulo Moggi <paulohpmoggi@gmail.com>
2022-05-23 23:30:27 -0300

    feat: Create irff calc

---
+++ app/__main__.py
@@ -0,0 +1,41 @@
+from decimal import Decimal
+import logging
+
+from dotenv import load_dotenv
+
+from app.utils import get_inss_tax, get_irff_tax
+
+load_dotenv(override=True)
+
+salaries = [
+    "2000",
+    "2500",
+    "2800",
+    "3000",
+    "3400",
+    "3800",
+    "4000",
+    "4400",
+    "4900",
+    "5000",
+    "5300",
+    "6000",
+    "7000",
+    "8000",
+]
+for salary in salaries:
+    inss_tax = get_inss_tax(salary=salary)
+    base_irff = Decimal(salary) - inss_tax
+    irff_tax = get_irff_tax(base_irff)
+    liquid = base_irff - irff_tax
+    stolen = inss_tax + irff_tax
+    logging.warning(
+        {
+            "Salary": salary,
+            "INSS tax": str(inss_tax),
+            "IRFF base": str(base_irff),
+            "IRFF tax": str(irff_tax),
+            "Stolen": str(stolen),
+            "Bottom line": str(liquid),
+        }
+    )
--- app/utils.py
+++ app/utils.py
@@ -33,40 +33,38 @@ def get_inss_tax(salary: Union[Decimal, str, int, float], inss_range_rate: dict[
     --------
     >>> get_inss_tax("1200")
     Decimal('90.00')
-    >>> get_inss_tax("1212")
-    Decimal('90.90')
-    >>> get_inss_tax("1212.89")
-    Decimal('90.98')
-    >>> get_inss_tax("1212.90")
-    Decimal('90.98')
-    >>> get_inss_tax("1300")
-    Decimal('98.82')
+    >>> get_inss_tax("1212.62")
+    Decimal('90.95')
+    >>> get_inss_tax("1212.63")
+    Decimal('90.96')
+    >>> get_inss_tax("2427.35")
+    Decimal('200.28')
+    >>> get_inss_tax("3641.03")
+    Decimal('345.92')
     >>> get_inss_tax("8070")
     Decimal('828.38')
     """
     salary = Decimal(salary)
     default_range_rate = {
+        "0": "0",
         "1212.00": "7.5",
         "2427.35": "9",
         "3641.03": "12",
         "7087.22": "14",
     }
     final_range_rate = inss_range_rate or default_range_rate
-    final_range_rate["-0.01"] = final_range_rate["-0.01"] if "-0.01" in final_range_rate else "0"
 
-    inss_range = sorted(list(map(Decimal, final_range_rate.keys())))
-    inss_rate = sorted(list(map(Decimal, final_range_rate.values())))
+    inss_range = list(map(Decimal, final_range_rate.keys()))
+    inss_rate = list(map(Decimal, final_range_rate.values()))
 
     residual = 0
     for index in range(1, len(inss_range)):
         range_value = inss_range[index]
         range_tax = inss_rate[index]
 
-        lower_range = inss_range[index - 1] + Decimal("0.01")
+        lower_range = Decimal("0") if index == 1 else inss_range[index - 1] + Decimal("0.01")
 
         if salary <= range_value:
-            if lower_range == Decimal("0.01"):
-                lower_range = Decimal("0")
 
             partial = (salary - lower_range) * range_tax / Decimal("100")
             residual += round_money(partial)
@@ -77,3 +75,36 @@ def get_inss_tax(salary: Union[Decimal, str, int, float], inss_range_rate: dict[
 
     final = round_money(residual)
     return final
+
+
+def get_irff_tax(salary: Union[Decimal, str, int, float]) -> Decimal:
+    """Utility function to retrieve IRFF tax based on salary.
+
+    Examples
+    --------
+    >>> get_irff_tax("1000")
+    Decimal('0.00')
+    >>> get_irff_tax("2473.65")
+    Decimal('42.72')
+    >>> get_irff_tax("3500.63")
+    Decimal('170.29')
+    >>> get_irff_tax("4664.68")
+    Decimal('413.42')
+    >>> get_irff_tax("8000")
+    Decimal('1330.64')
+    """
+    range_rates = {
+        "1903.98": ["0", "0"],
+        "2826.65": ["7.5", "142.80"],
+        "3751.05": ["15", "354.80"],
+        "4664.68": ["22.5", "636.13"],
+        "Infinity": ["27.5", "869.36"],
+    }
+    salary = Decimal(salary)
+    for range_value, range_rate in range_rates.items():
+        irrf_rate = Decimal(range_rate[0]) / Decimal("100")
+        irrf_deduction = Decimal(range_rate[1])
+
+        if salary <= Decimal(range_value):
+            final = salary * irrf_rate - irrf_deduction
+            return round_money(final)
